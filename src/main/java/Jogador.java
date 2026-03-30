9eae539f66762c5b8c31a1d7f3a150878579d52c
Joao Costa <zkozta@gmail.com>
2026-03-30 17:56:33 -0300

    classe Jogador pronta

--- src/main/java/Jogador.java
+++ src/main/java/Jogador.java
@@ -1,4 +1,58 @@
 package main.java;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Jogador {
+
+    private Jogador (){
+        this.inventario = new ArrayList<>();
+    };
+
+    private static Jogador instance = new Jogador();
+
+    public static Jogador getInstance() {
+        return instance;
+    };
+
+    private String nomeJogador;
+    private List<String> inventario;
+    private int qtdItens;
+    private int maxItens;
+
+
+    public String getNomeJogador() {
+        return nomeJogador;
+    };
+
+    public void setNomeJogador(String nomeJogador) {
+        this.nomeJogador = nomeJogador;
+    };
+
+    public List<String> getInventario() {
+        return inventario;
+    };
+
+    public void addItem(String item) {
+        if(this.inventario.size() < this.maxItens) {
+            this.inventario.add(item);
+            this.qtdItens = this.inventario.size();
+        } else {
+            System.out.println("Inventario Cheio");
+        }
+    }
+
+    public int getQtdItens() {
+        return qtdItens;
+    };
+
+    public int getMaxItens() {
+        return maxItens;
+    };
+
+    public void setMaxItens(int maxItens) {
+        this.maxItens = maxItens;
+    };
+
+
 }
